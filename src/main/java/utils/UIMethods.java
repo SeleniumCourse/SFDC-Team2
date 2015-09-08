886a767967485d3850f66bbcf3b8341c9d27d794
¨RubenBlancoBz¨ <ruben_arkaitz@hotmail.com>
2015-09-08 02:39:46 -0300

    updates for report and take screenshots

--- src/main/java/utils/UIMethods.java
+++ src/main/java/utils/UIMethods.java
@@ -8,21 +8,22 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-public class UIMethods {	
+public class UIMethods {
 
-	/** 
-	 * This method takes screenshots 
-	 * @param filePath 
-	 * @param fileName 
-	 * @throws IOException 
-	 */ 
-	public static void takeScreenShot(String filePath, String fileName) throws IOException { 
-		try { 
-			File scrFile = ((TakesScreenshot) DriverManager.getInstance().getDriver())
-					.getScreenshotAs(OutputType.FILE); 
-			FileUtils.copyFile(scrFile, new File(filePath + fileName)); 
-		} catch (Exception e) { 
-			e.printStackTrace(); 
-		} 
-	} 
+    /**
+     * This method takes screenshots
+     *
+     * @param filePath
+     * @param fileName
+     * @throws IOException
+     */
+    public static void takeScreenShot(String filePath, String fileName) throws IOException {
+        try {
+            File scrFile = ((TakesScreenshot) DriverManager.getInstance().getDriver())
+                    .getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File(filePath + fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
