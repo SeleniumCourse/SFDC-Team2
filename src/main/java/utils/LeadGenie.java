7ac5196862952e03532856cdb78b1f8803294d43
chelo <chelo.fm@gmail.com>
2015-09-07 22:12:20 -0400

    Fixing comments on LeadGenie

--- src/main/java/utils/LeadGenie.java
+++ src/main/java/utils/LeadGenie.java
@@ -18,8 +18,9 @@ public class LeadGenie {
     /**
      * Create a Lead object
      *
-     * @params campaignName: Campaign Name object to be created
-     * @return CampaignDetails object
+     * @params lastName: Last Name of Lead to be created
+     * @params Company: Company Name
+     * @return LeadDetails object
      */
     public static LeadDetails createLeadObject(String lastName, String company){
         Login login = new Login();
@@ -38,6 +39,4 @@ public class LeadGenie {
         leadDetails.clickDeleteButton(true);
         DriverManager.getInstance().getDriver().quit();
     }
-
-
 }
